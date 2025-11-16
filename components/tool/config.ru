a998367def74e6ab4e71fca8f74220b2bc7b2c0f
James Ryan <james@theryans.io>
2025-11-16 13:47:54 -0600

    chore: remove extracted components

--- components/tool/config.ru
+++
@@ -1,4 +0,0 @@
-require_relative 'server'
-
-# Completely bypass Rack::Protection by not using Sinatra's run! method
-run Langop::Server
--- components/tool/server.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-require "language_operator/tool_loader"
-
-# Start the MCP server using LanguageOperator SDK
-# This will load all tools from /mcp and start the server on PORT (default 80)
-LanguageOperator::ToolLoader.start
